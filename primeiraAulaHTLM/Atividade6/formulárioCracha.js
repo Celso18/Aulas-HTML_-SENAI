1f84dd1425e13693e820f336d2918e69979c62d4
Celso18 <celsoromao9@gmail.com>
2026-06-18 00:50:20 +0000

    Alteração do Cracha Virtual

---
+++ primeiraAulaHTLM/Atividade6/formulárioCracha.js
@@ -0,0 +1,15 @@
+let nome = document.getElementById("nome").value;
+let idade = document.getElementById("idade").value;
+let cargo = document.getElementById("cargo").value;
+let departamento = document.getElementById("departamento").value;
+let temAcessoRegistro = document.getElementById("acesso restrito").value;
+
+alert("cadastro do funcionario realizado com sucesso")
+
+console.log("") 
+console.log("nome") document.getElementById("nome").value;
+let idade = document.getElementById("idade").value;
+let cargo = document.getElementById("cargo").value;
+let departamento = document.getElementById("departamento").value;
+let temAcessoRegistro = document.getElementById("acesso restrito").value;
+
