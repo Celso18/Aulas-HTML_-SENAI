d7d7ed3012e99931fe2ed079cfcc63df882fd359
Celso18 <celsoromao9@gmail.com>
2026-06-23 00:45:50 +0000

    Atualizações

---
+++ Atividade7/persistente.js
@@ -0,0 +1,17 @@
+function salvarNome() {
+    let valorDigitado = document.getElementById("texto-inserido").value;
+    let idadeDigitada = 55;
+
+    let objetoNome = {
+        nome: valorDigitado,
+        idade: idadeDigitada
+    };
+
+    let textoJSON = JSON.stringify(objetoNome);
+    localStorage.setItem("dadoNome", textoJSON);
+
+    document.getElementById("nome-salvo").innerText = valorDigitado;
+}
+
+
+
