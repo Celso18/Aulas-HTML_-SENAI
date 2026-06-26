14a24d9d6039c9ba82b00e703d8510ef0fbbcc7e
Celso18 <celsoromao9@gmail.com>
2026-06-26 00:49:42 +0000

    Atualização

--- primeiraAulaHTLM/Atividade8/js/funcoes.js
+++ primeiraAulaHTLM/Atividade8/js/funcoes.js
@@ -1,54 +1,68 @@
-function soma(){
+function soma() {
     let primeiroNumero = document.getElementById("primeiro-numero").value;
     let segundoNumero = document.getElementById("segundo-numero").value;
 
     let resultado = Number(primeiroNumero) + Number(segundoNumero);
 
     document.getElementById("resultado").innerText = resultado;
-    salvarHistorico("soma", primeiroNumero, segundoNumero, resultado);
+
+    salvarHistorico("Soma", primeiroNumero, segundoNumero, resultado);
 }
 
-function subtracao(){
+function subtracao() {
     let primeiroNumero = document.getElementById("primeiro-numero").value;
     let segundoNumero = document.getElementById("segundo-numero").value;
 
     let resultado = Number(primeiroNumero) - Number(segundoNumero);
 
     document.getElementById("resultado").innerText = resultado;
-    salvarHistorico("subtração", primeiroNumero, segundoNumero, resultado);
+
+    salvarHistorico("Subtração", primeiroNumero, segundoNumero, resultado);
 }
 
-function multiplicacao(){
+function multiplicacao() {
     let primeiroNumero = document.getElementById("primeiro-numero").value;
     let segundoNumero = document.getElementById("segundo-numero").value;
 
     let resultado = Number(primeiroNumero) * Number(segundoNumero);
-    
-    document.getElementById("resultado").innerText = resultado;
-    salvarHistorico("multiplicação", primeiroNumero, segundoNumero, resultado);
-}   
 
-function divisao(){
+    document.getElementById("resultado").innerText = resultado;
+
+    salvarHistorico("Multiplicação", primeiroNumero, segundoNumero, resultado);
+}
+
+function divisao() {
     let primeiroNumero = document.getElementById("primeiro-numero").value;
     let segundoNumero = document.getElementById("segundo-numero").value;
 
     let resultado = Number(primeiroNumero) / Number(segundoNumero);
 
     document.getElementById("resultado").innerText = resultado;
-    salvarHistorico("divisão", primeiroNumero, segundoNumero, resultado);
-}   
 
-
-function salvarHistorico(funcao, primeiroNumero, segundoNumero, resultado) {
-    console.log(`nomeFunção: ${funcao}`);
-    console.log(`Primeiro numero: ${primeiroNumero}, SegundoNumero: ${segundoNumero}`);
-    console.log(`Resultado: ${resultado}`);
-    console.log("--------------------------------------------------");
+    salvarHistorico("Divisão", primeiroNumero, segundoNumero, resultado);
 }
 
+function salvarHistorico(nomeDaFuncao, num1, num2, resultado) {
+    console.log("Função: " + nomeDaFuncao);
+    console.log("Primeiro numero: " + num1 + ", SegundoNumero: " + num2);
+    console.log("Resultado: " + resultado);
+    console.log("--------------------------------------");
+
+    let operacao = {
+        funcao: nomeDaFuncao,
+        numero1: num1,
+        numero2: num2,
+        resultado: resultado
+    };
+
+    let historicoLocal = JSON.parse(localStorage.getItem("historicoCalculos")) || [];
+    historicoLocal.push(operacao);
+
+    localStorage.setItem("historicoCalculos", JSON.stringify(historicoLocal));
+}
 
  /*
-        Hisórico de função utilizada
+        Histórico de função utilizada
         Ex -> Função: soma
 
         Histórico de números digitados
@@ -59,3 +73,53 @@ function salvarHistorico(funcao, primeiroNumero, segundoNumero, resultado) {
 
         apresentado num console.log
     */
+
+        /*
+    Nova função de calculo de imposto de importação
+
+    Se o produto custar menos que $50 (cinquenta dolares)
+    Você vai ter que pagar o valor produto, mais 20%
+
+    Se o produto custar mais do que $50
+    Você vai ter que pagar o valor do produto +20%, depois mais 90% sobre o resultado
+
+    Seja salvo num novo objeto, dentro do local storage, seu hitórico de coversão.
+
+    Ex: histórico de calculos matemáticos, e histórico de conversões
+
+ */
+
+    function calcularImposto() {
+        let valorProduto = document.getElementById("valor-produto").value;
+        let resultadoImposto;
+
+        if (Number(valorProduto) < 50) {
+            resultadoImposto = Number(valorProduto) + (Number(valorProduto) * 0.20);
+        } else {
+            resultadoImposto = (Number(valorProduto) + (Number(valorProduto) * 0.20)) * 1.90;
+        }
+
+        document.getElementById("resultado-imposto").innerText = resultadoImposto;
+
+        salvarHistoricoImposto(valorProduto, resultadoImposto);
+
+    }
+
+    function salvarHistoricoImposto(valorProduto, resultadoImposto) {
+        console.log("Valor do produto: " + valorProduto);
+        console.log("Resultado do imposto: " + resultadoImposto);
+        console.log("--------------------------------------");
+
+        let imposto = {
+            valorProduto: valorProduto,
+            resultadoImposto: resultadoImposto
+        };
+
+        let historicoImpostoLocal = JSON.parse(localStorage.getItem("historicoImpostos")) || [];
+        historicoImpostoLocal.push(imposto);
+
+        localStorage.setItem("historicoImpostos", JSON.stringify(historicoImpostoLocal));
+        console.table(historico);
+    }
+ 
+    
